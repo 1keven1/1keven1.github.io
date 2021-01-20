37ffb04c7c3ff07994863a8798aaa56ea2c824c2
1keven1 <zhangsuzhe@sina.com>
2021-01-20 21:52:13 +0800

    Site updated: 2021-01-20 21:52:09

--- mysource/Effect.js
+++ mysource/Effect.js
@@ -1,40 +1,12 @@
-
-
 //鼠标移动特效
-/*
 (function () {
-    function t() {
-        i(), a()
-    }
-    function i() {
-        document.addEventListener("mousemove", o),
-            document.addEventListener("touchmove", e),
-            document.addEventListener("touchstart", e),
-            window.addEventListener("resize", n)
-    }
-    function n(t) {
-        d = window.innerWidth, window.innerHeight
-    }
-    function e(t) {
-        if (t.touches.length > 0)
-            for (var i = 0; i < t.touches.length; i++)
-                s(t.touches[i].clientX, t.touches[i].clientY, r[Math.floor(Math.random() * r.length)])
-    }
-    function o(t) {
-        u.x = t.clientX, u.y = t.clientY, s(u.x, u.y, r[Math.floor(Math.random() * r.length)])
-    }
-    function s(t, i, n) {
-        var e = new l; e.init(t, i, n), f.push(e)
-    }
-    function h() {
-        for (var t = 0; t < f.length; t++)f[t].update(); for (t = f.length - 1; t >= 0; t--)f[t].lifeSpan < 0 && (f[t].die(), f.splice(t, 1))
-    }
-    function a() {
-        requestAnimationFrame(a), h()
-    }
-    function l() {
-        this.character = "*",
-            this.lifeSpan = 120,
+    var colors = ["#D61C59", "#E7D84B", "#1B8798"];
+    elementGroup = [];
+    //元素类
+    class Element {
+        constructor() {
+            this.character = "❤";
+            this.lifeSpan = 120;
             this.initialStyles = {
                 position: "fixed",
                 top: "0",
@@ -42,33 +14,81 @@
                 pointerEvents: "none",
                 "z-index": "10000000",
                 fontSize: "20px",
-                "will-change": "transform"
-            },
-            this.init = function (t, i, n) {
-                this.velocity = { x: (Math.random() < .5 ? -1 : 1) * (Math.random() / 2), y: 1 },
-                    this.position = { x: t - 10, y: i - 20 },
-                    this.initialStyles.color = n, console.log(n),
-                    this.element = document.createElement("span"),
-                    this.element.innerHTML = this.character,
-                    c(this.element, this.initialStyles),
-                    this.update(),
-                    document.body.appendChild(this.element)
-            },
+                "will-change": "transform",
+                color: "#000000"
+            };
+            this.init = function (x, y, color) {
+                this.velocity = { x: (Math.random() < .5 ? -1 : 1) * (Math.random() / 2), y: 1 };
+                this.position = { x: x - 10, y: y - 20 };
+                this.initialStyles.color = color;
+                this.element = document.createElement("span");
+                this.element.innerHTML = this.character;
+                ApplyStyle(this.element, this.initialStyles);
+                this.update();
+                document.body.appendChild(this.element);
+            };
             this.update = function () {
-                this.position.x += this.velocity.x,
-                    this.position.y += this.velocity.y,
-                    this.lifeSpan--,
-                    this.element.style.transform = "translate3d(" + this.position.x + "px," + this.position.y + "px,0) scale(" + this.lifeSpan / 120 + ")"
-            },
+                this.position.x += this.velocity.x;
+                this.position.y += this.velocity.y;
+                this.lifeSpan--;
+                this.element.style.transform = "translate3d(" + this.position.x + "px," + this.position.y + "px,0) scale(" + this.lifeSpan / 120 + ")";
+            };
             this.die = function () {
-                this.element.parentNode.removeChild(this.element)
+                this.element.parentNode.removeChild(this.element);
+            };
+        }
+    }
+
+    AddListener();
+    Loop();
+    //添加事件监听器
+    function AddListener() {
+        //当前事件对象会作为第一个参数传入函数
+        document.addEventListener("mousemove", onMouseMove);
+        document.addEventListener("touchmove", Touch);
+        document.addEventListener("touchstart", Touch);
+    }
+    //循环
+    function Loop() {
+        requestAnimationFrame(Loop);
+        Rander();
+    }
+    //逐个渲染
+    function Rander() {
+        for (var i = 0; i < elementGroup.length; i++) {
+            elementGroup[i].update();
+            if(elementGroup[i].lifeSpan < 0)
+            {
+                elementGroup[i].die();
+                elementGroup.splice(i, 1);
             }
+        }
     }
-    function c(t, i) {
-        for (var n in i)
-            t.style[n] = i[n]
+    //鼠标移动事件函数
+    function onMouseMove(t) {
+        num = Math.floor(Math.random() * colors.length);
+        CreateElement(t.clientX, t.clientY, colors[num]);
     }
-    var r = ["#D61C59", "#E7D84B", "#1B8798"], d = window.innerWidth, u = (window.innerHeight, { x: d / 2, y: d / 2 }), f = [];
-    t()
+    //添加元素
+    function CreateElement(x, y, color) {
+        var e = new Element;
+        e.init(x, y, color);
+        elementGroup.push(e);
+    }
+    //调整元素属性
+    function ApplyStyle(element, style) {
+        for (var i in style) {
+            //console.log(n);
+            element.style[i] = style[i];
+        }
+    }
+    //触摸事件函数
+    function Touch(t) {
+        if (t.touches.length > 0) {
+            for (var i = 0; i < t.touches.length; i++) {
+                num=Math.floor(Math.random() * r.length);
+                s(t.touches[i].clientX, t.touches[i].clientY, r[num]);
+            }
+        }
+    }
 })();
-*/
